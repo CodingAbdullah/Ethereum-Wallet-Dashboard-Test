a18e5dc0ab10a0324bcfdb051b69ecbbcf7cec1e
Abdul <abdullahmd326@gmail.com>
2022-12-02 01:50:07 -0500

    ENS to Address xpath complete

--- selenium_test/src/main/java/selenium_test_pages/ENSToAddressPage.java
+++ selenium_test/src/main/java/selenium_test_pages/ENSToAddressPage.java
@@ -1,5 +1,83 @@
 package selenium_test_pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ENSToAddressPage {
 	// Represent ENS To Address Page as a model using Page Factory architecture
+	
+	@FindBy(xpath="//nav//form//input")
+	private WebElement navbarInputElement;
+	
+	@FindBy(xpath="//nav//form//button")
+	private WebElement navbarButtonElement;
+	
+	@FindBy(xpath="//main//div[@class='jumbotron']//div[@class='container']//form//input")
+	private WebElement addressInputElement;
+	
+	@FindBy(xpath="//main//div[@class='jumbotron']//div[@class='container']//form//button")
+	private WebElement lookupReversalButtonElement;
+	
+	@FindBy(xpath="//main//div[@class='jumbotron']//div[@class='container']//button[1]")
+	private WebElement homeButtonElement;
+	
+	@FindBy(xpath="//main//div[@class='jumbotron']//div[@class='container']//button[2]")
+	private WebElement clearButtonElement;
+	
+	
+	// Page Object Model in action for ENSToAddress page
+	public ENSToAddressPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setNavbarInputElement(String keys) {
+		this.navbarInputElement.sendKeys(keys);
+	}
+	
+	public void setAddressInputElement(String keys) {
+		this.addressInputElement.sendKeys(keys);
+	}
+	
+	public void clickNavbarButtonElement() {
+		this.navbarButtonElement.click();
+	}
+	
+	public void clickHomeButtonElement() {
+		this.homeButtonElement.click();
+	}
+	
+	public void clickClearButtonElement() {
+		this.clearButtonElement.click();
+	}
+	
+	public void clickLookupReversalButtonElement() {
+		this.lookupReversalButtonElement.click();
+	}
+	
+	public WebElement getNavbarInputElement() {
+		return this.navbarInputElement;
+	}
+	
+	public WebElement getNavbarButtonElement() {
+		return this.navbarButtonElement;
+	}
+	
+	public WebElement getAddressInputElement() {
+		return this.addressInputElement;
+	}
+	
+	public WebElement getLookupReversalButtonElement() {
+		return this.lookupReversalButtonElement;
+	}
+	
+	public WebElement getHomeButtonElement() {
+		return this.homeButtonElement;
+	}
+	
+	public WebElement getClearButtonElement() {
+		return this.clearButtonElement;
+	}
+	
 }
