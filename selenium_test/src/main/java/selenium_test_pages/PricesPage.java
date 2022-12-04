b883e5e84b5e4bf49262f1cc3b85770972fc127e
Abdul <abdullahmd326@gmail.com>
2022-12-04 01:23:30 -0500

    xpaths complete for prices page

--- selenium_test/src/main/java/selenium_test_pages/PricesPage.java
+++ selenium_test/src/main/java/selenium_test_pages/PricesPage.java
@@ -1,6 +1,63 @@
 package selenium_test_pages;
 
-public class PricesPage {
-	// Represent Prics Page as a model using Page Factory architecture
+import java.util.List;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PricesPage {
+	// Represent Prices Page as a model using Page Factory architecture
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//button")
+	private WebElement coinPricesButtonElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//main//h1")
+	private WebElement pricesPageTitleElement;
+	
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//main//p")
+	private WebElement pricesPageDescriptionElement;
+	
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//main//div[@class='row']")
+	private List<WebElement> coinPricesCardsElement;
+	
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//main//button")
+	private WebElement hideCoinPricesButtonElement;
+	
+	
+	public PricesPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickCoinPricesButtonElement() {
+		this.coinPricesButtonElement.click();
+	}
+	
+	public void clickHideCoinPricesButtonElement() {
+		this.hideCoinPricesButtonElement.click();
+	}
+	
+	public WebElement getCoinPricesButtonElement() {
+		return this.coinPricesButtonElement;
+	}
+	
+	public WebElement getHideCoinPricesButtonElement() {
+		return this.hideCoinPricesButtonElement;
+	}
+	
+	public WebElement getPricesPageTitleElement() {
+		return this.pricesPageTitleElement;
+	}
+	
+	public WebElement getPricesPageDescriptionElement() {
+		return this.pricesPageDescriptionElement;
+	}
+	
+	public List<WebElement> getCoinPricesCardElement(){
+		return this.coinPricesCardsElement;
+	}
 }
