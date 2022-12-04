dac8d8c48715888c73d3c1a23d54abc6ce8ab6c1
Abdul <abdullahmd326@gmail.com>
2022-12-04 06:51:17 -0500

    Added AnalyticsSelectionPage for selenium testing, xpaths complete

---
+++ selenium_test/src/main/java/selenium_test_pages/AnalyticsSelectionPage.java
@@ -0,0 +1,80 @@
+package selenium_test_pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AnalyticsSelectionPage {
+	// Represent ENS Lookups Page as a model using Page Factory architecture
+	
+	@FindBy(xpath="//nav//form//input")
+	private WebElement navbarInputElement;
+	
+	@FindBy(xpath="//nav//form//button")
+	private WebElement navbarButtonElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//div//main//div[1]//h1")
+	private WebElement analyticsTitleElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//div//main//div[2]//div[@class='container']//div//label[1]")
+	private WebElement tokenAnalyticsTitleElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//div//main//div[2]//div[@class='container']//div//label[2]")
+	private WebElement walletAnalyticsTitleElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//div//main//div[2]//div[@class='container']//div//button[1]")
+	private WebElement tokenAnalyticsButtonElement;
+	
+	@FindBy(xpath="//div[@class='App']//div[3]//div//main//div[2]//div[@class='container']//div//button[2]")
+	private WebElement walletAnalyticsButtonElement;
+	
+	// Page Object Model in action for ENS Lookups page
+	public AnalyticsSelectionPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setNavbarInputElement(String keys) {
+		this.navbarInputElement.sendKeys(keys);
+	}
+	
+	public void clickNavbarButtonElement() {
+		this.navbarButtonElement.click();
+	}
+	
+	public void clickTokenAnalyticsButtonElement() {
+		this.tokenAnalyticsButtonElement.click();
+	}
+	
+	public void clickWalletAnalyticsButtonElement() {
+		this.walletAnalyticsButtonElement.click();
+	}
+	
+	public WebElement getTokenAnalyticsButtonElement() {
+		return this.tokenAnalyticsButtonElement;
+	}
+	
+	public WebElement getWalletAnalyticsButtonElement() {
+		return this.walletAnalyticsButtonElement;
+	}
+	
+	public WebElement getTokenAnalyticsTitleElement() {
+		return this.tokenAnalyticsTitleElement;
+	}
+	
+	public WebElement getWalletAnalyticsTitleElement() {
+		return this.walletAnalyticsTitleElement;
+	}
+	
+	public WebElement getNavbarInputElement() {
+		return this.navbarInputElement;
+	}
+	
+	public WebElement getNavbarButtonElement() {
+		return this.navbarButtonElement;
+	}
+	
+	public WebElement getAnalyticsTitleElement() {
+		return this.analyticsTitleElement;
+	}
+}
