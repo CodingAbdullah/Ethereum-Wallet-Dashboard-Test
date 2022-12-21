569397bd193c993a580da45c5add4c4bad89ea13
Abdul <abdullahmd326@gmail.com>
2022-12-21 02:05:21 -0500

    Adding steps for generic testing

--- selenium_test/src/test/java/stepDefinitions/AboutPageStepDefinition.java
+++ selenium_test/src/test/java/stepDefinitions/AboutPageStepDefinition.java
@@ -1,5 +1,84 @@
 package stepDefinitions;
 
-public class AboutPageStepDefinition {
+import org.openqa.selenium.WebDriver;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.AboutPage;
+
+public class AboutPageStepDefinition {
+	private WebDriver driver;
+	private AboutPage AboutPageInstance;
+	
+	
+	@Given("User lands on About Page")
+	public void user_lands_on_About_Page() {
+		
+	}
+	
+	@Given("User lands on a particular page")
+	public void user_lands_on_a_particular_page() {
+		
+	}
+	
+	@When("User selects the developer information link")
+	public void user_selects_the_developer_information_link() {
+		
+	}
+	
+	@When("User selects the Go Home button")
+	public void user_selects_the_go_home_button() {
+		
+	}
+	
+	@When("User selects a footer link")
+	public void user_selects_a_footer_link() {
+		
+	}
+	
+	@When("User selects a certain link")
+	public void user_selects_a_certain_link() {
+		
+	}
+	
+	@When("User selects Navbar to enter an incorrect wallet address")
+	public void user_selects_navbar_to_enter_an_incorrect_wallet_address() {
+		
+	}
+	
+	@When("User selects Navbar to enter a correct wallet address")
+	public void user_selects_navbar_to_enter_a_correct_wallet_address() {
+		
+	}
+	
+	@Then("Redirect User to the developer's about page")
+	public void redirect_user_to_the_developers_about_page() {
+		
+	}
+	
+	@Then("Redirect User to the Home Page")
+	public void redirect_user_to_the_home_page() {
+		
+	}
+	
+	@Then("Redirect User to that link")
+	public void redirect_user_to_that_link() {
+		
+	}
+	
+	@Then("Redirect User to that page")
+	public void redirect_user_to_that_page() {
+		
+	}
+
+	@Then("Display alert warning User")
+	public void display_alert_warning_user() {
+		
+	}
+	
+	@Then("Redirect User to Transactions Page")
+	public void redirect_user_to_transactions_page() {
+		
+	}
 }
--- selenium_test/src/test/java/stepDefinitions/ERC721CollectionPageStepDefinition.java
+++ selenium_test/src/test/java/stepDefinitions/ERC721CollectionPageStepDefinition.java
@@ -1,5 +1,59 @@
 package stepDefinitions;
 
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.ERC721CollectionPage;
+
 public class ERC721CollectionPageStepDefinition {
 
+	private WebDriver driver;
+	private ERC721CollectionPage ERC721CollectionPageInstance;
+	
+	@Given("User lands on a particular page")
+	public void user_lands_on_a_particular_page() {
+		
+	}
+
+	@When("User selects a certain link")
+	public void user_selects_a_certain_link() {
+		
+	}
+
+	@When("User selects Navbar to enter an incorrect wallet address")
+	public void user_selects_navbar_to_enter_an_incorrect_wallet_address() {
+		
+	}
+
+	@When("User selects Navbar to enter a correct wallet address")
+	public void user_selects_navbar_to_enter_a_correct_wallet_address() {
+		
+	}
+
+	@When("User selects a footer link")
+	public void user_selects_a_footer_link() {
+		
+	}
+
+	@Then("Redirect User to that page")
+	public void redirect_user_to_that_page() {
+		
+	}
+
+	@Then("Redirect User to Transactions Page")
+	public void redirect_user_to_transactions_page() {
+		
+	}
+
+	@Then("Display alert warning User")
+	public void display_alert_warning_user() {
+		
+	}
+
+	@Then("Redirect User to that link")
+	public void redirect_user_to_that_link() {
+		
+	}
 }
